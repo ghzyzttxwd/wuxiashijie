64fb4fb385aa780ca7cb8bb01b50c032f18e31ad
ghzyzttxwd <2697941746@qq.com>
2026-08-30 13:09:27 +0800

    feat(m2): add Daming country data

---
+++ src/data/countries/ming.js
@@ -0,0 +1,7 @@
+export const MING_COUNTRY={
+  id:'ming',name:'大明',enabled:true,
+  description:'朝堂与江湖犬牙交错。护龙山庄、东厂、五岳剑派与日月神教都在此活动。',
+  capitalLocationId:'ming_capital',
+  starterLocationIds:['ming_capital','ming_luoyang'],
+  locationIds:['ming_capital','ming_luoyang','ming_huashan','ming_songshan','ming_fuzhou','ming_meizhuang','ming_heimuya']
+};
