ca7671383b459a520ca0762b15df1ce6995da9f0
ghzyzttxwd <2697941746@qq.com>
2026-08-30 15:33:43 +0800

    feat(m7): split Shaolin martial manuals

---
+++ src/data/items/manuals/shaolin.js
@@ -0,0 +1,6 @@
+const M=(id,name,martialId)=>({id,name,type:'manual',martialId});
+export const SHAOLIN_MARTIAL_MANUALS=[
+M('manual_shaolin_vajra_palm','《大力金刚掌谱》','martial_shaolin_vajra_palm'),
+M('manual_yijinjing','《易筋经》','martial_yijinjing'),
+M('manual_shaolin_golden_bell','《金钟罩秘本》','martial_shaolin_golden_bell')
+];
