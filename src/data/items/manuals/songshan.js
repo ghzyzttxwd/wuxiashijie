fea6a4149445c757e812c52e838a81c4eb9664d9
ghzyzttxwd <2697941746@qq.com>
2026-08-30 15:33:32 +0800

    feat(m7): add Songshan martial manuals

---
+++ src/data/items/manuals/songshan.js
@@ -0,0 +1,5 @@
+const M=(id,name,martialId)=>({id,name,type:'manual',martialId});
+export const SONGSHAN_MARTIAL_MANUALS=[
+M('manual_songshan_sword','《嵩山剑谱》','martial_songshan_sword'),
+M('manual_ice_qi','《寒冰真气秘要》','martial_ice_qi')
+];
