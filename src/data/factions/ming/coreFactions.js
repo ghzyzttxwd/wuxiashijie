8545404b081117d4b018f711b6f7f2d4519c570d
ghzyzttxwd <2697941746@qq.com>
2026-08-30 15:22:48 +0800

    feat(m6): add East Depot Sun Moon Cult and Plum Manor factions

--- src/data/factions/ming/coreFactions.js
+++ src/data/factions/ming/coreFactions.js
@@ -1,9 +1,12 @@
 const F=(id,name,type,homeLocationId)=>({id,name,type,homeLocationId});
 export const MING_CORE_FACTIONS=[
 F('faction_hulong','护龙山庄','court-jianghu','ming_capital'),
+F('faction_east_depot','东厂','court-intelligence','ming_capital'),
 F('faction_huashan','华山派','sect','ming_huashan'),
 F('faction_songshan','嵩山派','sect','ming_songshan'),
 F('faction_shaolin','少林','sect','ming_songshan'),
+F('faction_sunmoon','日月神教','sect-power','ming_heimuya'),
+F('faction_meizhuang','梅庄','hidden-manor','ming_meizhuang'),
 F('faction_fuwei','福威镖局','escort','ming_fuzhou')
 ];
 export const MING_CORE_FACTION_BY_ID=Object.fromEntries(MING_CORE_FACTIONS.map(x=>[x.id,x]));
