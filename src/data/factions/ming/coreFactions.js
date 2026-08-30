ffc47cd597fd877061678a18d89639043c590b02
ghzyzttxwd <2697941746@qq.com>
2026-08-30 13:14:16 +0800

    feat(m3): add stable-id Daming core factions

---
+++ src/data/factions/ming/coreFactions.js
@@ -0,0 +1,8 @@
+const F=(id,name,type,homeLocationId)=>({id,name,type,homeLocationId});
+export const MING_CORE_FACTIONS=[
+F('faction_hulong','护龙山庄','court-jianghu','ming_capital'),
+F('faction_huashan','华山派','sect','ming_huashan'),
+F('faction_songshan','嵩山派','sect','ming_songshan'),
+F('faction_fuwei','福威镖局','escort','ming_fuzhou')
+];
+export const MING_CORE_FACTION_BY_ID=Object.fromEntries(MING_CORE_FACTIONS.map(x=>[x.id,x]));
