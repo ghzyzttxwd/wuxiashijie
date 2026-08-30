7f50b6d738554e9e4ab48c3df2f126d60e7cc8d7
ghzyzttxwd <2697941746@qq.com>
2026-08-30 15:34:19 +0800

    refactor(m7): aggregate split martial manual packs

--- src/data/items/manuals/ming.js
+++ src/data/items/manuals/ming.js
@@ -1,7 +1,9 @@
-const M=(id,name,martialId)=>({id,name,type:'manual',martialId});
-export const MING_MARTIAL_MANUALS=[
-M('manual_huashan_basic_sword','《华山基础剑谱》','martial_huashan_basic_sword'),
-M('manual_zixia_gong','《紫霞秘笈》','martial_zixia_gong'),
-M('manual_shaolin_vajra_palm','《大力金刚掌谱》','martial_shaolin_vajra_palm')
-];
+import {COMMON_MARTIAL_MANUALS}from'./common.js';
+import {HUASHAN_MARTIAL_MANUALS}from'./huashan.js';
+import {SONGSHAN_MARTIAL_MANUALS}from'./songshan.js';
+import {SHAOLIN_MARTIAL_MANUALS}from'./shaolin.js';
+import {HULONG_MARTIAL_MANUALS}from'./hulong.js';
+import {SUNMOON_MARTIAL_MANUALS}from'./sunmoon.js';
+
+export const MING_MARTIAL_MANUALS=[...COMMON_MARTIAL_MANUALS,...HUASHAN_MARTIAL_MANUALS,...SONGSHAN_MARTIAL_MANUALS,...SHAOLIN_MARTIAL_MANUALS,...HULONG_MARTIAL_MANUALS,...SUNMOON_MARTIAL_MANUALS];
 export const MING_MARTIAL_MANUAL_BY_ID=Object.fromEntries(MING_MARTIAL_MANUALS.map(x=>[x.id,x]));
