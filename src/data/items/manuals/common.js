01bd5ae5460c74ee4d9a7ec035af90e61310d6d0
ghzyzttxwd <2697941746@qq.com>
2026-08-30 15:33:15 +0800

    feat(m7): add common martial manuals

---
+++ src/data/items/manuals/common.js
@@ -0,0 +1,5 @@
+const M=(id,name,martialId)=>({id,name,type:'manual',martialId});
+export const COMMON_MARTIAL_MANUALS=[
+M('manual_gale_fast_sword','《狂风快剑谱》','martial_gale_fast_sword'),
+M('manual_five_tigers_saber','《五虎断门刀谱》','martial_five_tigers_saber')
+];
