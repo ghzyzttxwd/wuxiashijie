9aca0b5a7d72cc706ebf70c51b1cbc167ab804d1
ghzyzttxwd <2697941746@qq.com>
2026-09-08 23:58:29 +0800

    feat: export fighter visual integration APIs

--- src/combat-render/index.js
+++ src/combat-render/index.js
@@ -1,2 +1,4 @@
 // Canvas表现层：动画、粒子、镜头、技能FX与战斗规则彻底分离。
-export const COMBAT_RENDER_VERSION=1;
+export const COMBAT_RENDER_VERSION=2;
+export {FIGHTER_VISUAL_STATES,createVisualState,visualStatesForEvent,applyVisualState} from './fighterVisualState.js';
+export {SpriteFighterProvider,createStaticFighterAsset} from './spriteFighterProvider.js';
