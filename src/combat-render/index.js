6d1a28cc06f791e3d95fa2c3d87acb4eb53df2a9
ghzyzttxwd <2697941746@qq.com>
2026-09-09 00:01:17 +0800

    feat: export 3D fighter provider

--- src/combat-render/index.js
+++ src/combat-render/index.js
@@ -1,4 +1,5 @@
 // Canvas表现层：动画、粒子、镜头、技能FX与战斗规则彻底分离。
-export const COMBAT_RENDER_VERSION=2;
+export const COMBAT_RENDER_VERSION=3;
 export {FIGHTER_VISUAL_STATES,createVisualState,visualStatesForEvent,applyVisualState} from './fighterVisualState.js';
 export {SpriteFighterProvider,createStaticFighterAsset} from './spriteFighterProvider.js';
+export {ModelFighterProvider,createModelFighterAsset} from './modelFighterProvider.js';
